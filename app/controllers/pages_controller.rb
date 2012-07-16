6b406bb532a1ac8284c2a7ba1cf37ab8a04a1d71
vikas yaligar <vikasyaligar.it@gmail.com>
2012-07-16 23:37:09 +0530

    done with slider

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     if signed_in?
       redirect_to overview_path(current_user)
     else
-      @all_images = ["box-logo.jpg","rails.png"].to_json.html_safe
+      @all_images = ["box-logo.jpg","rails.png","plannerace.jpg","logo.jpg"].to_json.html_safe
       @title = 'Home'
       @user = User.new
     end
