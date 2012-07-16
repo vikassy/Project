9696e60c3081a10eb8294ba57a405b8ce79d7b79
vikas yaligar <vikasyaligar.it@gmail.com>
2012-07-16 10:07:48 +0530

    Changed a bit of style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def home
-    @title = 'Home'
+    if signed_in?
+      redirect_to overview_path(current_user)
+    else
+      @all_images = ["box-logo.jpg","rails.png"].to_json.html_safe
+      @title = 'Home'
+      @user = User.new
+    end
   end
 
   def about
